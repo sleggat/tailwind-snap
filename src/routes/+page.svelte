7e2c308a5eb9ee8ba107bac957a366c6b5ca3175
sleggat <sleggat@gmail.com>
2025-01-25 10:44:56 +1300

    Trying to sort out redirect and canonical SEO issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,11 @@
 	// This page will only show briefly before redirecting
 </script>
 
+<svelte:head>
+	<meta property="og:url" content="https://tailwindcolorsnap.frontandback.co.nz" />
+	<link rel="canonical" href="https://tailwindcolorsnap.frontandback.co.nz" />
+</svelte:head>
+
 <div class="flex min-h-screen items-center justify-center bg-violet-50">
 	<div class="text-gray-500">Loading...</div>
 </div>
