94a96e90ba260e98274e6a0ae36b480ad7033bb0
sleggat <sleggat@gmail.com>
2025-01-25 10:49:37 +1300

    Removed randomness and change 303 > 301

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,6 +5,6 @@ import { getRandomHexColor } from '$lib/colors';
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
 	// Remove the # and ensure we get a 6-digit hex
-	const randomColor = getRandomHexColor().substring(1).padEnd(6, '0');
-	throw redirect(303, `/hex/${randomColor}`);
+	const randomColor = getRandomHexColor().substring(1).padEnd(6, '0'); // not used anymore
+	throw redirect(301, `/hex/ffc30e`);
 }
